c1aefc94e79eb3cfa38caddd4657496f70f99416
Anderson Borges Costa Ferreira <kisukeanderson@gmail.com>
2025-07-18 01:12:52 -0300

    Initial Commit

---
+++ Queima Pança/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Queima Pança
+//
+//  Created by Anderson Borges Costa Ferreira on 18/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Queima Pança/Queima_Panc_aApp.swift
@@ -0,0 +1,17 @@
+//
+//  Queima_Panc_aApp.swift
+//  Queima Pança
+//
+//  Created by Anderson Borges Costa Ferreira on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Queima_Panc_aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
